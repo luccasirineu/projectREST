fa0b689f25c77574bd15bddc141ca193beb777bb
luccasirineu <luccas19100@outlook.com>
2024-04-11 17:40:30 -0300

    Criação do ProjectRest

---
+++ src/main/java/com/luccasdev/springProject/ProjectRestApplication.java
@@ -0,0 +1,15 @@
+package com.luccasdev.springProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRestApplication.class, args);
+		
+		
+	}
+
+}
---
+++ src/test/java/com/luccasdev/springProject/ProjectRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luccasdev.springProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
