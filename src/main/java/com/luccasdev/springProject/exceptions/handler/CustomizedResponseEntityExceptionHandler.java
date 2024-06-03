13698a80234f2082cf534e839163e85f58fb7026
luccasirineu <luccas19100@outlook.com>
2024-06-03 16:12:15 -0300

    fix last commit

--- src/main/java/com/luccasdev/springProject/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/luccasdev/springProject/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -2,6 +2,7 @@ package com.luccasdev.springProject.exceptions.handler;
 
 import java.util.Date;
 
+import com.luccasdev.springProject.exceptions.InvalidJwtAuthenticationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -33,6 +34,16 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
-	
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationExceptions(
+			Exception ex, WebRequest request) {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
+	}
 
 }
