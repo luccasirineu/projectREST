b58ab78d66344019d69475d18bf7d194d64532ff
luccasirineu <luccas19100@outlook.com>
2024-06-04 14:11:51 -0300

    implementando o JwtConfigurer

--- src/main/java/com/luccasdev/springProject/security/JwtTokenFilter.java
+++ src/main/java/com/luccasdev/springProject/security/JwtTokenFilter.java
@@ -1,2 +1,37 @@
-package com.luccasdev.springProject.security;public class JwtTokenFilter {
+package com.luccasdev.springProject.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
+
+public class JwtTokenFilter extends GenericFilterBean {
+
+    @Autowired
+    TokenJwtProvider tokenJwtProvider;
+
+    public JwtTokenFilter(TokenJwtProvider tokenJwtProvider) {
+        this.tokenJwtProvider = tokenJwtProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        String token = tokenJwtProvider.resolveToken((HttpServletRequest)request); // obtendo token
+        if (token != null && tokenJwtProvider.validateToken(token)) { // validando token
+            Authentication authentication = tokenJwtProvider.getAuthentication(token); // obtem autenticação
+            if (authentication != null) {
+                SecurityContextHolder.getContext().setAuthentication(authentication); // setando a autenticação no ContextHolder do Spring
+            }
+        }
+        chain.doFilter(request, response);
+
+    }
 }
